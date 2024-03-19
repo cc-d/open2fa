40998eed477ca612161173df187224fb86c82750
Cary Carter <ccarterdev@gmail.com>
2024-03-19 06:18:33 -0700

    much improved Tests

--- tests.py
+++ tests.py
@@ -2,17 +2,18 @@ import pytest as pt
 from io import StringIO
 from unittest.mock import patch, MagicMock
 
-import sys
 import os
 import os.path as osp
+from shutil import rmtree
 from typing import Union as U, Generator as Gen
 from uuid import UUID, uuid4
-from open2fa.cli import Open2FA, main
+from open2fa.cli import Open2FA, main, sys
 from open2fa.main import apireq
 from open2fa.common import TOTP2FACode, RemoteSecret, O2FAUUID, TOTPSecret
 from open2fa import ex as EX
 from open2fa import msgs as MSGS
 from open2fa.version import __version__
+from open2fa.cli_utils import parse_cli_arg_aliases as pargs
 
 # Assuming ranstr function exists for generating random strings
 from pyshared import ranstr
@@ -44,29 +45,36 @@ _ENC_SECRETS = [
 ]
 
 
-@pt.fixture
+@pt.fixture(scope='module')
 def ranuuid():
     """Fixture to generate a random UUID for testing."""
     yield str(uuid4())
 
 
-@pt.fixture
+@pt.fixture(scope='module')
 def randir():
     """Fixture to generate a random directory path for testing."""
     _tmpdir = '/tmp/' + ranstr(10)
-    os.mkdir(_tmpdir, exist_ok=True)
+    if not osp.exists(_tmpdir):
+        os.mkdir(_tmpdir)
 
     yield _tmpdir
+    if osp.exists(_tmpdir):
 
-    os.rmdir(_tmpdir)
+        # delete the directory
+
+        # ensure it si deleted even if ti is not empty
+        rmtree(_tmpdir, ignore_errors=True)
 
 
-@pt.fixture
-def local_client():
+@pt.fixture(scope='module')
+def local_client(ranuuid: str, randir: str):
     """Fixture to create a TOTPSecret instance for testing."""
-    o2fa = Open2FA(_DIR, None, _URL)
-    if len(o2fa.secrets) == 0:
-        for sec in _SECRETS:
+    o2fa = Open2FA(
+        o2fa_dir=randir, o2fa_uuid=ranuuid, o2fa_api_url='http://test'
+    )
+    for sec in _SECRETS:
+        if sec not in o2fa.secrets:
             o2fa.add_secret(sec[0], sec[1])
     yield o2fa
 
@@ -81,30 +89,31 @@ def remote_client(local_client: Open2FA):
     yield local_client
 
 
-def exec_cmd(cmd: list[str], client: Open2FA) -> tuple[Open2FA, str]:
-    """Execute the command and return the output."""
-    with patch('sys.argv', cmd), patch(
-        'sys.stdout', new_callable=StringIO
-    ) as mock_out:
-        o2fa = main(
-            o2fa_dir=client.dir,
-            o2fa_uuid=client.uuid,
-            o2fa_api_url=client.api_url,
-            return_open2fa=True,
-        )
-        return o2fa.refresh(), mock_out.getvalue()
+from logfunc import logf
 
 
-@pt.mark.parametrize(
-    'cmd', [['open2fa', 'list'], ['open2fa', 'list', '-s'], ['open2fa', 'l']]
-)
+def exec_cmd(cmd, local_client: U[Open2FA, None] = None) -> U[Open2FA, Gen]:
+    cmd = ['cli.py'] + [str(c) for c in cmd]
+    client = local_client if local_client else Open2FA()
+    with patch('open2fa.cli_utils.parse_cli_arg_aliases') as mock_pargs:
+        mock_pargs.return_value = cmd
+        with patch('sys.argv', cmd):
+            with patch('sys.stdout', new_callable=StringIO) as out:
+                with patch('sys.stderr', new_callable=StringIO) as err:
+                    main(
+                        o2fa_api_url=client.api_url,
+                        o2fa_uuid=client.uuid,
+                        o2fa_dir=client.dir,
+                    )
+
+                    return client, out
+
+
+@pt.mark.parametrize('cmd', [['list'], ['list', '-s']])
 def test_list_cmd(cmd: list[str], local_client: Open2FA):
-    """Test the list command."""
-    o2fa, out = exec_cmd(cmd, local_client)
-    print('o2fa', o2fa, '\n\n', 'local_client', local_client, sep='\n')
+    o2fa, out = exec_cmd(cmd, local_client=local_client)
     for sec in _SECRETS:
         if '-s' in cmd:
-            assert sec[0] in out
+            assert sec[0] in out.getvalue()
         else:
-            assert sec[0][0] + '...' in out
-        assert str(sec[1]) in out
+            assert sec[0] not in out.getvalue()
